cf23aaed373e0c592f106b5f58aad20c2dbe72c0
Brant Wellman <BrantWellman@gmail.com>
2015-12-04 08:45:41 -0700

    Feature and unit tests passing. Full functionality - forms working

--- test/features/user_creates_robot_test.rb
+++ test/features/user_creates_robot_test.rb
@@ -6,20 +6,17 @@ class UserCreatesRobotFromNewForm < FeatureTest
     visit '/robots/new'
     fill_in('robot[name]', with: "Brant")
     fill_in('robot[city]', with: 'Denver')
-    # fill_in('robot[state]', with: 'Colorado')
-    fill_in('robot[birthdate]', with: '7777')
-    fill_in('robot[date_hired]', with: '8888')
-    # fill_in('robot[department]', with: 'education')
+    select('Colorado', from: 'robot[state]')
+    select('Education', from: "robot[department]")
     click_button('submit')
+
     visit '/robots'
 
     within ('.card') do
       assert page.has_content?("Brant")
       assert page.has_content?("Denver")
-      assert page.has_content?("Colorado")
-      assert page.has_content?('7777')
-      assert page.has_content?('8888')
-      assert page.has_content?('education')
+      assert page.has_content?("CO")
+      assert page.has_content?('Education')
     end
   end
 end
---
+++ test/features/user_deletes_existing_robot_test.rb
@@ -0,0 +1,27 @@
+require_relative '../test_helper'
+
+class UserDeletesRobotFromExistingTask < FeatureTest
+
+  def create_robots(num)
+    num.times do |n|
+      RobotManager.create({ :name => "a name#{n+1}",
+                            :city => "a city#{n+1}",
+                            :state => "a state#{n+1}",
+                            :birthdate => "a birthdate#{n+1}",
+                            :date_hired => "a date_hired#{n+1}",
+                            :department => "a department#{n+1}"
+                            })
+    end
+  end
+
+  def test_deleted_robot_is_not_displayed
+    create_robots(2)
+
+    visit '/robots/1'
+    click_button('Destroy Robot')
+    assert_equal '/robots', current_path
+    within ('.card') do
+      refute page.has_content?("1")
+    end
+  end
+end
